e25773136153e463974173aa0d6877687561967b
Zachie du Bruyn <zachie@webunite.co.za>
2013-03-05 00:28:19 -0800

    Moved spec files and added Apllication class stubs

---
+++ spec/Library/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\phpCodeWeaver\Library;
+
+include_once(dirname(__DIR__) . '/../src/phpCodeWeaver/Config/Shared.php');
+include_once(dirname(__DIR__) . '/../src/phpCodeWeaver/Library/Routing.php');
+
+use PHPSpec2\ObjectBehavior;
+
+class Application extends ObjectBehavior
+{
+	public function it_should_load_url()
+	{
+		$this::Load('test/test/tree/1')->shouldReturn(true);
+		$this::Controller()->shouldReturn('Test');
+		$this::Action()->shouldReturn('Test');
+		$this::Request()->shouldReturn(array('tree' => '1'));
+	}
+
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('phpCodeWeaver\Library\Routing');
+    }
+}
